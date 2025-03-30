829c50b5b0ac46acd0fcb5c302255752fb0d4f9b
ElizabethKirstein <106555381+ElizabethKirstein@users.noreply.github.com>
2025-03-30 09:23:20 -0400

    Update bash script

--- imports.bash
+++ imports.bash
@@ -1,3 +1,3 @@
-sudo easy_install pip
-sudo apt-get install espeak
-python3 -m pip install phonemizer requests
+sudo apt-get install python3-pip
+sudo apt-get install espeak-ng
+sudo python3 -m pip install phonemizer requests
